22c237a34821e3f719bbb61e214455c153c9efbc
viktoriya <viktoriyam356@gmail.com>
2023-02-23 18:04:36 +0300

    задача 2

---
+++ 02/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Number 1 = ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Number 2 = ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1; 
+
+if (number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+Console.WriteLine("Max = " + max);
+
+
