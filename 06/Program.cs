e606a45deb7dfb2ba1a97fc7968ae4a4d75152e7
viktoriya <viktoriyam356@gmail.com>
2023-02-23 18:31:00 +0300

    задача 6

---
+++ 06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Your number = ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 ==  0)
+{
+    Console.WriteLine("Yes ^_^");
+}
+else
+{
+    Console.WriteLine("No *_*");
+}
